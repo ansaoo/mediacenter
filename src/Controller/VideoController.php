cc6a6c0f48431691cbef19cec30a92278eb222ba
ansaoo <spin.github@outlook.com>
2018-06-03 23:19:34 +0200

    Update image cropper

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -9,7 +9,23 @@
 namespace App\Controller;
 
 
-class VideoController
-{
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
+class VideoController extends Controller
+{
+    public function movies(Request $request)
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY',null,'Unable to access this page!');
+        $user = $this->getUser();
+        return $this->render('video/movies.html.twig', array(
+            'menu' => array(
+                'video' => array(
+                    'li' => 'active',
+                    'ul' => null,
+                    'movie' => 'active'
+                )
+            )
+        ));
+    }
 }
