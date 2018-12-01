c008685013d075ddc3ff852aa93b9db0567a4ac8
ansaoo <spin.github@outlook.com>
2018-12-01 23:43:16 +0100

    Update picture entity with tag manytomany

--- src/Entity/Picture.php
+++ src/Entity/Picture.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -67,9 +69,14 @@ class Picture
     private $lastUpdate;
 
     /**
-     * @ORM\Column(type="array", nullable=true)
+     * @ORM\ManyToMany(targetEntity="App\Entity\Tag", inversedBy="pictures")
      */
-    private $tag = [];
+    private $tag;
+
+    public function __construct()
+    {
+        $this->tag = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -196,14 +203,28 @@ class Picture
         return $this;
     }
 
-    public function getTag(): ?array
+    /**
+     * @return Collection|Tag[]
+     */
+    public function getTag(): Collection
     {
         return $this->tag;
     }
 
-    public function setTag(?array $tag): self
+    public function addTag(Tag $tag): self
     {
-        $this->tag = $tag;
+        if (!$this->tag->contains($tag)) {
+            $this->tag[] = $tag;
+        }
+
+        return $this;
+    }
+
+    public function removeTag(Tag $tag): self
+    {
+        if ($this->tag->contains($tag)) {
+            $this->tag->removeElement($tag);
+        }
 
         return $this;
     }
---
+++ src/Entity/Tag.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TagRepository")
+ */
+class Tag
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Picture", mappedBy="tag")
+     */
+    private $pictures;
+
+    public function __construct()
+    {
+        $this->pictures = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Collection|Picture[]
+     */
+    public function getPictures(): Collection
+    {
+        return $this->pictures;
+    }
+
+    public function addPicture(Picture $picture): self
+    {
+        if (!$this->pictures->contains($picture)) {
+            $this->pictures[] = $picture;
+            $picture->addTag($this);
+        }
+
+        return $this;
+    }
+
+    public function removePicture(Picture $picture): self
+    {
+        if ($this->pictures->contains($picture)) {
+            $this->pictures->removeElement($picture);
+            $picture->removeTag($this);
+        }
+
+        return $this;
+    }
+}
