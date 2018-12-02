7f05144853d0894a8a81204178b9312c0814d592
ansaoo <spin.github@outlook.com>
2018-12-02 13:42:09 +0100

    Create picture controller

---
+++ src/Controller/PictureController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PictureController extends AbstractController
+{
+    /**
+     * @Route("/picture", name="picture")
+     */
+    public function index()
+    {
+        return $this->render('picture/index.html.twig', [
+            'controller_name' => 'PictureController',
+        ]);
+    }
+}
