413bf1ae168dfb7f1599350563f9562c0d9d31ca
Samuel Lecomte <samuel.lecomte@osmoz-it.com>
2018-12-02 15:53:37 +0100

    Add AbstractFormType implement

--- src/Form/UploadTaskType.php
+++ src/Form/UploadTaskType.php
@@ -10,11 +10,12 @@ namespace App\Form;
 
 
 use App\Entity\UploadTask;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class UploadTaskType
+class UploadTaskType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
