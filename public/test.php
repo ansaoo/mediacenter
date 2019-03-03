754f342c58b35c742f43b8bc47d78b44fbc5522a
Samuel Lecomte <samuel.lecomte@osmoz-it.com>
2019-03-03 16:53:37 +0100

    add phpinfo page

---
+++ public/test.php
@@ -0,0 +1,9 @@
+<?php
+
+//$filename = "2018/2018-02/2018-02-15_16h50m37_DSC02635.JPG";
+//echo dirname(dirname($filename));
+//echo '<br>';
+//echo basename(dirname($filename));
+//echo '<br>';
+//echo basename($filename);
+echo phpinfo();
